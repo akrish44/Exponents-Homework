40f864e454750afe08332105f38f59b07d82a79b
Susmita <akrish44@aol.com>
2013-09-08 16:20:46 -0600

    Calculated the exponent value of x^n power

--- Hailstone.java
+++ Hailstone.java
@@ -8,7 +8,7 @@ public class Hailstone {
 		System.out.println(str1);
 		int newNum = reader.nextInt();
 	
-	while () {
+	
 		while (newNum != 1 && newNum % 2 == 0) { {
 			newNum = newNum/2;
 			System.out.println(newNum);
@@ -21,6 +21,6 @@ public class Hailstone {
 		if (newNum == 1) {
 			System.out.println("Calculation stopped.");
 		} }
-		}
+		
 	}
 }
--- LoopsWarmup.java
+++ LoopsWarmup.java
@@ -1,9 +1,9 @@
 public class LoopsWarmup {
 
 	public static void main(String[] args) {
-		int n = 5;
-		int x = 1;
-		// exponent(2,3);
+		// int n = 5;
+		// int x = 1;
+		exponent(2,3);
 
 
 	}
@@ -12,24 +12,25 @@ public class LoopsWarmup {
 	// 	int sum = 1;
 	// 	for (int i=1; i <=n; i++) {
 	// 		sum+=i;
-	// 				}
+	// 	}
 	// 	System.out.println(sum);
 	// 	return sum;
 	// }
-	// public static int exponent(int x, int n) {
-	// 	int exponent = 1;
-	// 	for (int i = n; i > 0; i--) {
-	// 		exponent= exponent * x;
-	// 	}
-	// 	System.out.println(x + " to the power of " + n "equals " + exponent);
-	// }
-	
-	public static int factorial(int n) {
-		int factorial=1;
-		for(int i=1; i <=n; i++) {
-			factorial = factorial * i;
+	public static int exponent(int x, int n) {
+		int exponent = 1;
+		for (int i = n; i > 0; i--) {
+			exponent= exponent * x;
 		}
-		System.out.println("The factorial of " + n + "equals " + factorial);
-		return factorial;
+		System.out.println(exponent);
+		return exponent;
 	}
+	// public static int factorial(int n) {
+	// 	int factorial=1;
+	// 	for(int i=1; i <=n; i++) {
+	// 		factorial = factorial * i;
+		
+	// }
+	// System.out.println(factorial);
+	// return factorial;
+	// }
 }
