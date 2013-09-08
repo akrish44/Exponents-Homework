f01c23f26172308f9a499ed0244e4fde0720128e
Susmita <susmitakrishnaswamy@Mama-Aparnas-MacBook-Pro.local>
2013-09-08 11:43:37 -0600

    Initial commit

---
+++ Exponents.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Exponents {
+
+	Scanner x = new Scanner(System.in);
+	
+	double i = x.nextInt();
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter number: " + x);
+
+	}	
+}
---
+++ Exponents2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Exponents2  {
+
+public static void main(String[] args) {
+	
+		System.out.println("Enter a number: ");
+		Scanner reader = new Scanner (System.in);
+		long x = reader.nextLong();
+		long n = 1;
+		double sum = 1.0;
+		//double sum = 0.0;
+
+
+		double lessThan = Math.pow(10, -12);
+		double term = 7;
+		long fact = 1;
+		System.out.println("Limit value=" + lessThan);
+		while (term >= lessThan)
+		{
+
+			fact = 1;
+			for (long k = 1; k <= n; k++) {
+				fact = (fact*k);
+			} 
+			//System.out.println();
+
+			double ex=Math.pow(x,n);
+			term = ex / fact;
+			sum = sum + term;
+			System.out.println("n=" + n + "; Factorial= " + fact + "; Term=" + term + "; Power= " + ex + "; Sum= " + sum );
+			n++;
+		}
+		System.out.println("Real: " + Math.exp(x));
+	}	
+}
